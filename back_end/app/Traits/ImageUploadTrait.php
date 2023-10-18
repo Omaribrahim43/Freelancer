206b4868cb87f7db7e7e7fbbe0bc5631403b88dc
Omaribrahim43 <omar.migdadi43@gmail.com>
2023-10-18 11:27:00 +0300

    omar

--- back_end/app/Traits/ImageUploadTrait.php
+++ back_end/app/Traits/ImageUploadTrait.php
@@ -17,7 +17,7 @@ trait ImageUploadTrait
             $imageName = 'media_' . uniqid() . '.' . $ext;
             $image->move(public_path($path), $imageName);
 
-            return $path . "/" . $imageName;
+            return asset($path . "/" . $imageName);
         }
     }
 
